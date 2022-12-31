2aa3f82bfe08dfbe782dd357a58c1ccfd3c88d9f
ardacdx <116028863+ardacdx@users.noreply.github.com>
2022-12-31 22:57:25 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+require()
+
+module.exports = {
+    dsc: {
+        token: process.env.TOKEN
+    },
+
+    app: {
+        prefix: '!'
+    }
+ TOKEN=MTA1ODgzMTYwMDg5NDM2NTgyNw.GQXMGZ.cXgNlz7iF1B81sUW77RVaQAVX6CRquwwgYdWzM
+};
